d6bb26fbee9c74e2062462b1c526cf6be33d88bd
Tim Starling <tstarling@users.mediawiki.org>
2008-06-14 09:32:06 +0000

    Ran a new vulnerability scanner on the extensions directory. Found the following:
    * register_globals XSS vulnerabilities in: Call, ChangeAuthor, EditOwn, SignDocument, TemplateLink, WatchSubpages, WhoIsWatching, php/ext/MediaWiki
    * register_globals arbitrary inclusion vulnerabilities in: CategoryIntersection, Makebot, PasswordReset, SemanticCalendar, SemanticForms, SemanticMediaWiki, SocialProfile, SpamRegex, StalePages, TodoTasks, WhiteList, Wikidata, regexBlock
    * Plain (non-register_globals) XSS vulnerabilities in: geo, MetavidWiki, wikihiero
    * Parser warnings in: SyntaxHighlight_vim, SpamDiffTool, TidyTab
    
    Fixed all. Some fixes are lightly tested.

--- SocialProfile.php
+++ SocialProfile.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * Protect against register_globals vulnerabilities.
+ * This line must be present before any global variable is referenced.
+ */
+if (!defined('MEDIAWIKI')) die();
 
 $dir = dirname(__FILE__) . '/';
 
