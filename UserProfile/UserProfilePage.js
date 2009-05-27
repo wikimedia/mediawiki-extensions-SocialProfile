da19f1095a98ccfb412a2d0c00359b763ddf96f0
Jack Phoenix <ashley@users.mediawiki.org>
2009-05-27 09:07:16 +0000

    SocialProfile: rewrote some js bits to be less dependent on YUI

--- UserProfile/UserProfilePage.js
+++ UserProfile/UserProfilePage.js
@@ -1,30 +1,30 @@
+/**
+ * JavaScript functions used by UserProfile
+ */
 var posted = 0;
 function send_message(){
-	if($("message").value && !posted){
+	if( document.getElementById('message').value && !posted ){
 		posted = 1;
-		var url = "index.php?action=ajax";
-		var pars = 'rs=wfSendBoardMessage&rsargs[]=' + $("user_name_to").value +'&rsargs[]=' + encodeURIComponent($("message").value) + '&rsargs[]=' + $("message_type").value +'&rsargs[]=10'
-		var callback = {
-			success: function(originalRequest){
-				$("user-page-board").innerHTML = originalRequest.responseText
+		sajax_request_type = 'POST';
+		sajax_do_call( 'wfSendBoardMessage', [
+			document.getElementById('user_name_to').value,
+			encodeURIComponent( document.getElementById('message').value ),
+			document.getElementById('message_type').value,
+			10 ], function( originalRequest ){
+				document.getElementById('user-page-board').innerHTML = originalRequest.responseText;
 				posted = 0;
-				$("message").value='';
+				document.getElementById('message').value = '';
 			}
-		};
-		var request = YAHOO.util.Connect.asyncRequest('POST', url, callback, pars);
+		);
 	}
 }
 
-function delete_message(id){
-	if(confirm('Are you sure you want to delete this message?')){
-		var url = "index.php?action=ajax";
-		var pars = 'rs=wfDeleteBoardMessage&rsargs[]=' + id
-		var callback = {
-			success: function(originalRequest){
-				window.location.reload();
-			}
-		};
-		var request = YAHOO.util.Connect.asyncRequest('POST', url, callback, pars);
+function delete_message( id ){
+	if( confirm( 'Are you sure you want to delete this message?' ) ){
+		sajax_request_type = 'POST';
+		sajax_do_call( 'wfDeleteBoardMessage', [ id ], function( originalRequest ){
+			window.location.reload();
+		} );
 	}
 }
 
@@ -37,66 +37,65 @@ function showUploadFrame(){
 	new YAHOO.widget.Effects.Show('upload-container');
 }
 
-function uploadError(message){
-	$('mini-gallery-' + replaceID).innerHTML = oldHtml;
-	$('upload-frame-errors').innerHTML = message;
-	$('imageUpload-frame').src = 'index.php?title=Special:MiniAjaxUpload&wpThumbWidth=75';
+function uploadError( message ){
+	document.getElementById('mini-gallery-' + replaceID).innerHTML = oldHtml;
+	document.getElementById('upload-frame-errors').innerHTML = message;
+	document.getElementById('imageUpload-frame').src = 'index.php?title=Special:MiniAjaxUpload&wpThumbWidth=75';
 
 	new YAHOO.widget.Effects.Show('upload-container');
 }
 
-function textError(message){
-	$('upload-frame-errors').innerHTML = message;
+function textError( message ){
+	document.getElementById('upload-frame-errors').innerHTML = message;
 	new YAHOO.widget.Effects.Show('upload-frame-errors');
 }
 
 function completeImageUpload(){
 	new YAHOO.widget.Effects.Hide('upload-frame-errors');
-	$('upload-frame-errors').innerHTML = '';
-	oldHtml = $('mini-gallery-' + replaceID).innerHTML;
+	document.getElementById('upload-frame-errors').innerHTML = '';
+	oldHtml = document.getElementById('mini-gallery-' + replaceID).innerHTML;
 
-	for(x=7;x>0;x--){
-		$('mini-gallery-' + (x) ).innerHTML = $('mini-gallery-' + (x-1) ).innerHTML.replace('slideShowLink('+(x-1)+')','slideShowLink('+(x)+')')
+	for( x = 7; x > 0; x-- ){
+		document.getElementById('mini-gallery-' + (x) ).innerHTML = document.getElementById('mini-gallery-' + (x-1) ).innerHTML.replace('slideShowLink('+(x-1)+')','slideShowLink('+(x)+')');
 	}
-	$('mini-gallery-0').innerHTML ='<a><img height=\"75\" width=\"75\" src=\"http://images.wikia.com/common/wikiany/images/ajax-loader-white.gif\"/></a>';
+	document.getElementById('mini-gallery-0').innerHTML = '<a><img height="75" width="75" src="http://images.wikia.com/common/wikiany/images/ajax-loader-white.gif" alt="" /></a>';
 
 	//new YAHOO.widget.Effects.Hide('mini-gallery-nopics');
-	if($('no-pictures-containers')) {
-		new YAHOO.widget.Effects.Hide('no-pictures-containers');
+	if( document.getElementById( 'no-pictures-containers' ) ) {
+		new YAHOO.widget.Effects.Hide( 'no-pictures-containers' );
 	}
-	new YAHOO.widget.Effects.Show('pictures-containers');
+	new YAHOO.widget.Effects.Show( 'pictures-containers' );
 }
 
-function uploadComplete(imgSrc, imgName, imgDesc){
+function uploadComplete( imgSrc, imgName, imgDesc ){
 	replaceSrc = imgSrc;
 
-	$('upload-frame-errors').innerHTML = '';
+	document.getElementById('upload-frame-errors').innerHTML = '';
 
-	//$('imageUpload-frame').onload = function(){
+	//document.getElementById('imageUpload-frame').onload = function(){
 		var idOffset = -1 - numReplaces;
 		//$D.addClass('mini-gallery-0','mini-gallery');
-		//$('mini-gallery-0').innerHTML = '<a href=\"javascript:slideShowLink(' + idOffset + ')\">' + replaceSrc + '</a>';
-		$('mini-gallery-0').innerHTML = '<a href=\"' + __image_prefix + imgName + '\">' + replaceSrc + '</a>';
+		//document.getElementById('mini-gallery-0').innerHTML = '<a href=\"javascript:slideShowLink(' + idOffset + ')\">' + replaceSrc + '</a>';
+		document.getElementById('mini-gallery-0').innerHTML = '<a href=\"' + __image_prefix + imgName + '\">' + replaceSrc + '</a>';
 
 		//replaceID = (replaceID == 7) ? 0 : (replaceID + 1);
 		numReplaces += 1;
 
 	//}
-	//if ($('imageUpload-frame').captureEvents) $('imageUpload-frame').captureEvents(Event.LOAD);
+	//if (document.getElementById('imageUpload-frame').captureEvents) document.getElementById('imageUpload-frame').captureEvents(Event.LOAD);
 
-	$('imageUpload-frame').src = 'index.php?title=Special:MiniAjaxUpload&wpThumbWidth=75&extra=' + numReplaces;
+	document.getElementById('imageUpload-frame').src = 'index.php?title=Special:MiniAjaxUpload&wpThumbWidth=75&extra=' + numReplaces;
 }
 
-function slideShowLink(id){
+function slideShowLink( id ){
 	//window.location = 'index.php?title=Special:UserSlideShow&user=' + __slideshow_user + '&picture=' + ( numReplaces + id );
 	window.location = 'Image:' + id;
 }
 
-function doHover(divID) {
+function doHover( divID ) {
 	$El(divID).setStyle('backgroundColor', '#4B9AF6');
-
 }
 
-function endHover(divID){
+function endHover( divID ){
 	$El(divID).setStyle('backgroundColor', '');
 }
