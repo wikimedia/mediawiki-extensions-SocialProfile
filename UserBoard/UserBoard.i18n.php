1bdca80270a34859102305db19474317da73e488
Raimond Spekking <raymond@users.mediawiki.org>
2009-10-28 19:20:11 +0000

    Localisation updates for core and extension messages from translatewiki.net (2009-10-28 18:30 UTC)

--- UserBoard/UserBoard.i18n.php
+++ UserBoard/UserBoard.i18n.php
@@ -1444,7 +1444,11 @@ a změń swoje nastajenja, zo by e-mejlowe zdźělenja znjemóžnił.',
  * @author Glanthor Reviol
  */
 $messages['hu'] = array(
+	'boardblastlogintitle' => 'Be kell jelentkezned hírek küldéséhez.',
+	'boardblastlogintext' => 'Ahhoz hogy tudj híreket küldeni,
+<a href="index.php?title=Special:UserLogin">be kell jelentkezned</a>.',
 	'messagesenttitle' => 'Elküldött üzenetek',
+	'boardblasttitle' => 'Hír küldése',
 	'boardblaststep1' => '1. lépés – írd meg az üzeneted',
 	'boardblastprivatenote' => 'Minden üzenet privát üzenetként lesz elküldve',
 	'boardblaststep2' => '2. lépés – válaszd ki, kinek szeretnéd elküldeni az üzenetet',
@@ -1454,11 +1458,23 @@ $messages['hu'] = array(
 	'boardlinkunselectfriends' => 'Barátok kijelölésének megszüntetése',
 	'boardlinkselectfoes' => 'Ellenségek kijelölése',
 	'boardlinkunselectfoes' => 'Ellenségek kijelölésének megszüntetése',
+	'boardsendbutton' => 'Hír küldése hirdetőfalra',
+	'boardnofriends' => 'Nincsenek barátaid, akiknek üzenetet küldhetnél!',
 	'messagesentsuccess' => 'Az üzeneted sikeresen elküldve',
+	'userboard' => 'Hirdetőfal',
+	'userboard_board-to-board' => 'Hirdetőfaltól-hirdetőfalig',
 	'userboard_delete' => 'Törlés',
 	'userboard_noexist' => 'A megtekinteni próbált felhasználó nem létezik.',
+	'userboard_yourboard' => 'Saját hirdetőfalad',
+	'userboard_owner' => '$1 hirdetőfala',
+	'userboard_yourboardwith' => 'Hirdetőfalak közti beszélgetésed vele: $1',
+	'userboard_otherboardwith' => '$1 hirdetőfalak közti beszélgetése vele: $2',
+	'userboard_backprofile' => 'Vissza $1 profiljára',
+	'userboard_backyourprofile' => 'Vissza a profilodra',
+	'userboard_boardtoboard' => 'Hirdetőfal-beszélgetés',
 	'userboard_confirmdelete' => 'Biztos vagy benne, hogy törölni szeretnéd ezt az üzenetet?',
 	'userboard_sendmessage' => 'Üzenet küldése $1 részére',
+	'userboard_myboard' => 'Saját hirdetőfalam',
 	'userboard_posted_ago' => 'beküldve: $1',
 	'userboard_private' => 'privát',
 	'userboard_public' => 'nyilvános',
@@ -1467,10 +1483,27 @@ $messages['hu'] = array(
 	'userboard_prevpage' => 'előző',
 	'userboard_nomessages' => 'Nincsenek üzenetek.',
 	'userboard_sendbutton' => 'küldés',
+	'userboard_loggedout' => '<a href="$1">Be kell jelentkezned</a> üzenetek küldéséhez más felhasználóknak.',
+	'userboard_showingmessages' => '{{PLURAL:$4|$3 üzenet|$2 – $3 üzenetek}} megjelenítése (összesen: $1)',
+	'right-userboard-delete' => 'mások hirdetőfal-üzeneteinek törlése',
 	'userboard-time-days' => '{{PLURAL:$1|egy|$1}} nap',
 	'userboard-time-hours' => '{{PLURAL:$1|egy|$1}} óra',
 	'userboard-time-minutes' => '{{PLURAL:$1|egy|$1}} perc',
 	'userboard-time-seconds' => '{{PLURAL:$1|egy|$1}} másodperc',
+	'message_received_subject' => '$1 írt a hirdetőfaladra a(z) {{SITENAME}} oldalon',
+	'message_received_body' => 'Szia $1!
+
+$2 írt a hirdetőfaladra a(z) {{SITENAME}} oldalon!
+
+Kattints alább a hirdetőfalad megtekintéséhez.
+
+$3
+
+~~
+Szeretnéd ha nem zaklatnánk több e-maillel?
+
+Kattints a linkre: $4
+és tiltsd le az e-mail értesítéseket a beállításaidban',
 );
 
 /** Interlingua (Interlingua)
