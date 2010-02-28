e05298a4e6ad80155e89fa4b4209dc41e8f63443
Jack Phoenix <ashley@users.mediawiki.org>
2010-02-28 21:27:38 +0000

    SocialProfile: forgot one SystemGifts file

--- SystemGifts/UserSystemGiftsClass.php
+++ SystemGifts/UserSystemGiftsClass.php
@@ -28,16 +28,19 @@ class UserSystemGifts {
 	public function sendSystemGift( $gift_id, $email = true ) {
 		global $wgMemc;
 
-		if ( $this->doesUserHaveGift( $this->user_id, $gift_id ) ) return '';
+		if ( $this->doesUserHaveGift( $this->user_id, $gift_id ) ) {
+			return '';
+		}
 
 		$dbw = wfGetDB( DB_MASTER );
-		$dbw->insert( 'user_system_gift',
+		$dbw->insert(
+			'user_system_gift',
 			array(
 				'sg_gift_id' => $gift_id,
 				'sg_user_id' => $this->user_id,
 				'sg_user_name' => $this->user_name,
 				'sg_status' => 1,
-				'sg_date' => date( "Y-m-d H:i:s" ),
+				'sg_date' => date( 'Y-m-d H:i:s' ),
 			),
 			__METHOD__
 		);
@@ -47,17 +50,19 @@ class UserSystemGifts {
 		// Add to new gift count cache for receiving user
 		$this->incNewSystemGiftCount( $this->user_id );
 
-		if ( $email && !empty( $sg_gift_id ) ) $this->sendGiftNotificationEmail( $this->user_id, $gift_id );
+		if ( $email && !empty( $sg_gift_id ) ) {
+			$this->sendGiftNotificationEmail( $this->user_id, $gift_id );
+		}
 		$wgMemc->delete( wfMemcKey( 'user', 'profile', 'system_gifts', $this->user_id ) );
 		return $sg_gift_id;
 	}
 
 	public function sendGiftNotificationEmail( $user_id_to, $gift_id ) {
-		wfLoadExtensionMessages( 'SystemGifts' );
 		$gift = SystemGifts::getGift( $gift_id );
 		$user = User::newFromId( $user_id_to );
 		$user->loadFromDatabase();
 		if ( $user->isEmailConfirmed() && $user->getIntOption( 'notifygift', 1 ) ) {
+			wfLoadExtensionMessages( 'SystemGifts' );
 			$gifts_link = SpecialPage::getTitleFor( 'ViewSystemGifts' );
 			$update_profile_link = SpecialPage::getTitleFor( 'UpdateProfile' );
 			$subject = wfMsgExt( 'system_gift_received_subject', 'parsemag',
@@ -77,7 +82,12 @@ class UserSystemGifts {
 
 	public function doesUserHaveGift( $user_id, $gift_id ) {
 		$dbr = wfGetDB( DB_SLAVE );
-		$s = $dbr->selectRow( 'user_system_gift', array( 'sg_status' ), array( 'sg_user_id' => $user_id, 'sg_gift_id' => $gift_id ), __METHOD__ );
+		$s = $dbr->selectRow(
+			'user_system_gift',
+			array( 'sg_status' ),
+			array( 'sg_user_id' => $user_id, 'sg_gift_id' => $gift_id ),
+			__METHOD__
+		);
 		if ( $s !== false ) {
 			return true;
 		}
@@ -87,11 +97,9 @@ class UserSystemGifts {
 	public function clearAllUserSystemGiftStatus() {
 		$dbw = wfGetDB( DB_MASTER );
 		$dbw->update( 'user_system_gift',
-			array( /* SET */
-			'sg_status' => 0
-			), array( /* WHERE */
-			'sg_user_id' => $this->user_id
-			), __METHOD__
+		/* SET */array( 'sg_status' => 0 ),
+		/* WHERE */array( 'sg_user_id' => $this->user_id ),
+			__METHOD__
 		);
 		$this->clearNewSystemGiftCountCache( $this->user_id );
 	}
@@ -99,17 +107,20 @@ class UserSystemGifts {
 	static function clearUserGiftStatus( $id ) {
 		$dbw = wfGetDB( DB_MASTER );
 		$dbw->update( 'user_system_gift',
-			array( /* SET */
-			'sg_status' => 0
-			), array( /* WHERE */
-			'sg_id' => $id
-			), __METHOD__
+			/* SET */array( 'sg_status' => 0 ),
+			/* WHERE */array( 'sg_id' => $id ),
+			__METHOD__
 		);
 	}
 
 	public function doesUserOwnGift( $user_id, $sg_id ) {
 		$dbr = wfGetDB( DB_SLAVE );
-		$s = $dbr->selectRow( 'user_system_gift', array( 'sg_user_id' ), array( 'sg_id' => $sg_id ), __METHOD__ );
+		$s = $dbr->selectRow(
+			'user_system_gift',
+			array( 'sg_user_id' ),
+			array( 'sg_id' => $sg_id ),
+			__METHOD__
+		);
 		if ( $s !== false ) {
 			if ( $user_id == $s->ug_user_id_to ) {
 				return true;
@@ -125,11 +136,20 @@ class UserSystemGifts {
 
 	static function getUserGift( $id ) {
 		$dbr = wfGetDB( DB_SLAVE );
-		$sql = "SELECT sg_id, sg_user_id, sg_user_name,gift_id, sg_date,
-			gift_name, gift_description, gift_given_count, sg_status
-			FROM {$dbr->tableName( 'user_system_gift' )} INNER JOIN {$dbr->tableName( 'system_gift' )} ON sg_gift_id=gift_id
-			WHERE sg_id = {$id} LIMIT 0,1";
-		$res = $dbr->query( $sql );
+		$res = $dbr->select(
+			array( 'user_system_gift', 'system_gift' ),
+			array(
+				'sg_id', 'sg_user_id', 'sg_user_name', 'gift_id', 'sg_date',
+				'gift_name', 'gift_description', 'gift_given_count', 'sg_status'
+			),
+			array( "sg_id = {$id}" ),
+			__METHOD__,
+			array(
+				'LIMIT' => 1,
+				'OFFSET' => 0
+			),
+			array( 'system_gift' => array( 'INNER JOIN', 'sg_gift_id = gift_id' ) )
+		);
 		$row = $dbr->fetchObject( $res );
 		if ( $row ) {
 			$gift['id'] = $row->sg_id;
@@ -193,8 +213,15 @@ class UserSystemGifts {
 		$key = wfMemcKey( 'system_gifts', 'new_count', $user_id );
 		$dbr = wfGetDB( DB_SLAVE );
 		$new_gift_count = 0;
-		$s = $dbr->selectRow( 'user_system_gift', array( 'count(*) AS count' ), array( 'sg_user_id' => $user_id, 'sg_status' => 1 ), __METHOD__ );
-		if ( $s !== false ) $new_gift_count = $s->count;
+		$s = $dbr->selectRow(
+			'user_system_gift',
+			array( 'COUNT(*) AS count' ),
+			array( 'sg_user_id' => $user_id, 'sg_status' => 1 ),
+			__METHOD__
+		);
+		if ( $s !== false ) {
+			$new_gift_count = $s->count;
+		}
 
 		$wgMemc->set( $key, $new_gift_count );
 
@@ -204,22 +231,32 @@ class UserSystemGifts {
 	public function getUserGiftList( $type, $limit = 0, $page = 0 ) {
 		$dbr = wfGetDB( DB_SLAVE );
 
+		$limitvalue = 0;
 		if ( $limit > 0 ) {
-			$limitvalue = 0;
-			if ( $page ) $limitvalue = $page * $limit - ( $limit );
-			$limit_sql = " LIMIT {$limitvalue},{$limit} ";
+			if ( $page ) {
+				$limitvalue = $page * $limit - ( $limit );
+			}
 		}
 
-		$sql = "SELECT sg_id, sg_user_id, sg_user_name, sg_gift_id, sg_date, sg_status,
-			gift_name, gift_description, gift_given_count, UNIX_TIMESTAMP(sg_date) AS unix_time
-			FROM {$dbr->tableName( 'user_system_gift' )} INNER JOIN {$dbr->tableName( 'system_gift' )} ON sg_gift_id=gift_id
-			WHERE sg_user_id = {$this->user_id}
-			ORDER BY sg_id DESC
-			{$limit_sql}";
+		$res = $dbr->select(
+			array( 'user_system_gift', 'system_gift' ),
+			array(
+				'sg_id', 'sg_user_id', 'sg_user_name', 'sg_gift_id', 'sg_date',
+				'sg_status', 'gift_name', 'gift_description',
+				'gift_given_count', 'UNIX_TIMESTAMP(sg_date) AS unix_time'
+			),
+			array( "sg_user_id = {$this->user_id}" ),
+			__METHOD__,
+			array(
+				'ORDER BY' => 'sg_id DESC',
+				'LIMIT' => $limit,
+				'OFFSET' => $limitvalue
+			),
+			array( 'system_gift' => array( 'INNER JOIN', 'sg_gift_id = gift_id' ) )
+		);
 
-		$res = $dbr->query( $sql );
 		$requests = array();
-		while ( $row = $dbr->fetchObject( $res ) ) {
+		foreach ( $res as $row ) {
 			$requests[] = array(
 				'id' => $row->sg_id,
 				'gift_id' => $row->sg_gift_id,
@@ -248,11 +285,13 @@ class UserSystemGifts {
 	static function getGiftCountByUsername( $user_name ) {
 		$dbr = wfGetDB( DB_SLAVE );
 		$user_id = User::idFromName( $user_name );
-		$sql = "SELECT count(*) AS count
-			FROM {$dbr->tableName( 'user_system_gift' )}
-			WHERE sg_user_id = {$user_id}
-			LIMIT 0,1";
-		$res = $dbr->query( $sql );
+		$res = $dbr->select(
+			'user_system_gift',
+			array( 'COUNT(*) AS count' ),
+			array( "sg_user_id = {$user_id}" ),
+			__METHOD__,
+			array( 'LIMIT' => 1, 'OFFSET' => 0 )
+		);
 		$row = $dbr->fetchObject( $res );
 		$gift_count = 0;
 		if ( $row ) {
