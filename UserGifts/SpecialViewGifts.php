8436c7d442f99de414a7256600a32cba8f77c6e6
Jack Phoenix <ashley@users.mediawiki.org>
2011-04-06 13:45:42 +0000

    SocialProfile: remove some unused variables and rename some badly named variables in SpecialViewGifts.php

--- UserGifts/SpecialViewGifts.php
+++ UserGifts/SpecialViewGifts.php
@@ -1,9 +1,16 @@
 <?php
+/**
+ * Special:ViewGifts -- a special page for viewing the list of user-to-user
+ * gifts a given user has received
+ *
+ * @file
+ * @ingroup Extensions
+ */
 
 class ViewGifts extends SpecialPage {
 
 	/**
-	 * Constructor
+	 * Constructor -- set up the new special page
 	 */
 	public function __construct() {
 		parent::__construct( 'ViewGifts' );
@@ -15,12 +22,12 @@ class ViewGifts extends SpecialPage {
 	 * @param $par Mixed: parameter passed to the page or null
 	 */
 	public function execute( $par ) {
-		global $wgUser, $wgOut, $wgRequest, $wgMemc, $wgUploadPath, $wgUserGiftsScripts;
+		global $wgUser, $wgOut, $wgRequest, $wgUploadPath, $wgUserGiftsScripts;
 
 		$wgOut->addExtensionStyle( $wgUserGiftsScripts . '/UserGifts.css' );
 
 		$user_name = $wgRequest->getVal( 'user' );
-		$page =  $wgRequest->getVal( 'page' );
+		$page = $wgRequest->getVal( 'page' );
 
 		/**
 		 * Redirect Non-logged in users to Login Page
@@ -33,14 +40,13 @@ class ViewGifts extends SpecialPage {
 		}
 
 		/**
-		 * If no user is set in the URL, we assume its the current user
+		 * If no user is set in the URL, we assume it's the current user
 		 */
 		if ( !$user_name ) {
 			$user_name = $wgUser->getName();
 		}
 		$user_id = User::idFromName( $user_name );
 		$user = Title::makeTitle( NS_USER, $user_name );
-		$user_safe = urlencode( $user_name );
 
 		/**
 		 * Error message for username that does not exist (from URL)
@@ -68,66 +74,76 @@ class ViewGifts extends SpecialPage {
 		$gifts = $rel->getUserGiftList( 0, $per_page, $page );
 		$total = $rel->getGiftCountByUsername( $user_name );
 
-		$relationship = UserRelationship::getUserRelationshipByID( $user_id, $wgUser->getID() );
-
 		/**
 		 * Show gift count for user
 		 */
-		$output = $wgOut->setPageTitle( wfMsg( 'g-list-title', $rel->user_name ) );
+		$wgOut->setPageTitle( wfMsg( 'g-list-title', $rel->user_name ) );
 
-		$output .= '<div class="back-links">
-			<a href="' . $user->getFullURL() . '">'
-				. wfMsg( 'g-back-link', $rel->user_name ) .
+		$output = '<div class="back-links">
+			<a href="' . $user->getFullURL() . '">' .
+				wfMsg( 'g-back-link', $rel->user_name ) .
 			'</a>
 		</div>
-		<div class="g-count">'
-			. wfMsgExt( 'g-count', 'parsemag', $rel->user_name, $total ) .
+		<div class="g-count">' .
+			wfMsgExt( 'g-count', 'parsemag', $rel->user_name, $total ) .
 		'</div>';
 
 		if ( $gifts ) {
 			$x = 1;
 
 			// Safe links
-			$view_gift_link = SpecialPage::getTitleFor( 'ViewGift' );
-			$give_gift_link = SpecialPage::getTitleFor( 'GiveGift' );
-			$remove_gift_link = SpecialPage::getTitleFor( 'RemoveGift' );
+			$viewGiftLink = SpecialPage::getTitleFor( 'ViewGift' );
+			$giveGiftLink = SpecialPage::getTitleFor( 'GiveGift' );
+			$removeGiftLink = SpecialPage::getTitleFor( 'RemoveGift' );
 
 			foreach ( $gifts as $gift ) {
 				$giftname_length = strlen( $gift['gift_name'] );
 				$giftname_space = stripos( $gift['gift_name'], ' ' );
 
 				if ( ( $giftname_space == false || $giftname_space >= "30" ) && $giftname_length > 30 ) {
-					$gift_name_display = substr( $gift['gift_name'], 0, 30 ) . ' ' . substr( $gift['gift_name'], 30, 50 );
+					$gift_name_display = substr( $gift['gift_name'], 0, 30 ) .
+						' ' . substr( $gift['gift_name'], 30, 50 );
 				} else {
 					$gift_name_display = $gift['gift_name'];
 				}
 
 				$user_from = Title::makeTitle( NS_USER, $gift['user_name_from'] );
-				$gift_image = "<img src=\"{$wgUploadPath}/awards/" . Gifts::getGiftImage( $gift['gift_id'], 'l' ) . '" border="0" alt="" />';
+				$gift_image = "<img src=\"{$wgUploadPath}/awards/" .
+					Gifts::getGiftImage( $gift['gift_id'], 'l' ) .
+					'" border="0" alt="" />';
 
 				$output .= '<div class="g-item">
-					<a href="' . $view_gift_link->escapeFullURL( 'gift_id=' . $gift['id'] ) . '">' . $gift_image . '</a>
+					<a href="' . $viewGiftLink->escapeFullURL( 'gift_id=' . $gift['id'] ) . '">' .
+						$gift_image .
+					'</a>
 					<div class="g-title">
-						<a href="' . $view_gift_link->escapeFullURL( 'gift_id=' . $gift['id'] ) . '">' . $gift_name_display . '</a>';
+						<a href="' . $viewGiftLink->escapeFullURL( 'gift_id=' . $gift['id'] ) . '">' .
+							$gift_name_display .
+						'</a>';
 				if ( $gift['status'] == 1 ) {
 					if ( $user_name == $wgUser->getName() ) {
 						$rel->clearUserGiftStatus( $gift['id'] );
 						$rel->decNewGiftCount( $wgUser->getID() );
 					}
-					$output .= '<span class="g-new">' . wfMsg( 'g-new' ) . '</span>';
+					$output .= '<span class="g-new">' .
+						wfMsg( 'g-new' ) .
+					'</span>';
 				}
 				$output .= '</div>';
 
-				$output .= '<div class="g-from">'
-					. wfMsg( 'g-from', $user_from->escapeFullURL(), $gift['user_name_from'] ) .
+				$output .= '<div class="g-from">' .
+					wfMsg( 'g-from', $user_from->escapeFullURL(), $gift['user_name_from'] ) .
 				'</div>
 					<div class="g-actions">
-						<a href="' . $give_gift_link->escapeFullURL( 'gift_id=' . $gift['gift_id'] ) . '">' . wfMsg( 'g-to-another' ) . '</a>';
+						<a href="' . $giveGiftLink->escapeFullURL( 'gift_id=' . $gift['gift_id'] ) . '">' .
+							wfMsg( 'g-to-another' ) .
+						'</a>';
 				if ( $rel->user_name == $wgUser->getName() ) {
 					$output .= '&#160;';
-					$output .= wfMsgExt( 'pipe-separator' , 'escapenoentities' );
+					$output .= wfMsgExt( 'pipe-separator', 'escapenoentities' );
 					$output .= '&#160;';
-					$output .= '<a href="' . $remove_gift_link->escapeFullURL( 'gift_id=' . $gift['id'] ) . '">' . wfMsg( 'g-remove-gift' ) . '</a>';
+					$output .= '<a href="' . $removeGiftLink->escapeFullURL( 'gift_id=' . $gift['id'] ) . '">' .
+						wfMsg( 'g-remove-gift' ) . '</a>';
 				}
 				$output .= '</div>
 					<div class="cleared"></div>';
@@ -145,12 +161,13 @@ class ViewGifts extends SpecialPage {
 		 */
 		$numofpages = $total / $per_page;
 
-		$page_link = SpecialPage::getTitleFor( 'ViewGifts' );
+		$pageLink = SpecialPage::getTitleFor( 'ViewGifts' );
 
 		if ( $numofpages > 1 ) {
 			$output .= '<div class="page-nav">';
 			if ( $page > 1 ) {
-				$output .= '<a href="' . $page_link->escapeFullURL( 'user=' . $user_name . '&rel_type=' . $rel_type . '&page=' . ( $page - 1 ) ) . '">' . wfMsg( 'g-previous' ) . '</a> ';
+				$output .= '<a href="' . $pageLink->escapeFullURL( 'user=' . $user_name . '&page=' . ( $page - 1 ) ) . '">' .
+					wfMsg( 'g-previous' ) . '</a> ';
 			}
 
 			if ( ( $total % $per_page ) != 0 ) {
@@ -167,19 +184,17 @@ class ViewGifts extends SpecialPage {
 				if ( $i == $page ) {
 					$output .= ( $i . ' ' );
 				} else {
-					$output .= '<a href="' . $page_link->escapeFullURL( 'user=' . $user_name . '&rel_type=' . $rel_type . '&page=' . $i ) . "\">$i</a> ";
+					$output .= '<a href="' . $pageLink->escapeFullURL( 'user=' . $user_name . '&page=' . $i ) . "\">$i</a> ";
 				}
 			}
 
 			if ( ( $total - ( $per_page * $page ) ) > 0 ) {
-				$output .= ' <a href="' . $page_link->escapeFullURL( 'user=' . $user_name . '&rel_type=' . $rel_type . '&page=' . ( $page + 1 ) ) . '">' . wfMsg( 'g-next' ) . '</a>';
+				$output .= ' <a href="' . $pageLink->escapeFullURL( 'user=' . $user_name . '&page=' . ( $page + 1 ) ) . '">' .
+					wfMsg( 'g-next' ) . '</a>';
 			}
 			$output .= '</div>';
 		}
 
-		/**
-		 * Build next/prev nav
-		 */
 		$wgOut->addHTML( $output );
 	}
 }
