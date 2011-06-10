30950236763fc6b505e0333a49d474096ef66ce0
Jack Phoenix <ashley@users.mediawiki.org>
2011-06-10 09:08:34 +0000

    SocialProfile: remove backticks and add some comments to UserStatus' SQL file

--- UserStatus/userstatus.sql
+++ UserStatus/userstatus.sql
@@ -1,7 +1,12 @@
-CREATE TABLE /*_*/`user_status` (
+CREATE TABLE /*_*/user_status (
+  -- Unique status ID number
   `us_id` int(11) NOT NULL  DEFAULT 0 PRIMARY KEY,
+  -- ID number of the user who wrote this status update
   `us_user_id` int(11) NOT NULL default '0',
+  -- Timestamp of the status update
   `us_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  -- Name of the person who wrote the status update
   `us_user_text` varchar(255) NOT NULL default '',
+  -- The text of the status update
   `us_status` varchar(140) NOT NULL default ''
 )/*$wgDBTableOptions*/;
