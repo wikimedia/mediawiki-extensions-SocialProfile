8471276c078a19fb30106cc98060082067d564be
Raimond Spekking <raymond@users.mediawiki.org>
2011-07-04 20:11:46 +0000

    Localisation updates for core and extension messages from translatewiki.net (2011-07-04 19:53:00 UTC)

--- UserStatus/UserStatus.i18n.php
+++ UserStatus/UserStatus.i18n.php
@@ -18,14 +18,64 @@ $messages['en'] = array(
 	'userstatus-history' => "History",
 );
 
+/** German (Deutsch)
+ * @author Kghbln
+ */
+$messages['de'] = array(
+	'userstatus-edit' => 'Bearbeiten',
+	'userstatus-save' => 'Speichern',
+	'userstatus-cancel' => 'Abbrechen',
+	'userstatus-history' => 'Versionen',
+);
+
+/** Galician (Galego)
+ * @author Toliño
+ */
+$messages['gl'] = array(
+	'userstatus-edit' => 'Editar',
+	'userstatus-save' => 'Gardar',
+	'userstatus-cancel' => 'Cancelar',
+	'userstatus-history' => 'Historial',
+);
+
+/** Interlingua (Interlingua)
+ * @author McDutchie
+ */
+$messages['ia'] = array(
+	'userstatus-edit' => 'Modificar',
+	'userstatus-save' => 'Salveguardar',
+	'userstatus-cancel' => 'Cancellar',
+	'userstatus-history' => 'Historia',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'userstatus-edit' => 'Уреди',
+	'userstatus-save' => 'Зачувај',
+	'userstatus-cancel' => 'Откажи',
+	'userstatus-history' => 'Историја',
+);
+
+/** Portuguese (Português)
+ * @author Hamilton Abreu
+ */
+$messages['pt'] = array(
+	'userstatus-edit' => 'Editar',
+	'userstatus-save' => 'Gravar',
+	'userstatus-cancel' => 'Cancelar',
+	'userstatus-history' => 'Histórico',
+);
+
 /** Russian (Русский)
  * @author Yevhenii Vlasenko
  */
 $messages['ru'] = array(
 	'userstatus-edit' => 'Изменить',
-	'userstatus-save' => "Сохранить",
-	'userstatus-cancel' => "Отмена",
-	'userstatus-history' => "История",
+	'userstatus-save' => 'Сохранить',
+	'userstatus-cancel' => 'Отмена',
+	'userstatus-history' => 'История',
 );
 
 /** Ukrainian (Українська)
@@ -33,7 +83,8 @@ $messages['ru'] = array(
  */
 $messages['uk'] = array(
 	'userstatus-edit' => 'Редагувати',
-	'userstatus-save' => "Зберегти",
-	'userstatus-cancel' => "Відмінити",
-	'userstatus-history' => "Історія",
+	'userstatus-save' => 'Зберегти',
+	'userstatus-cancel' => 'Відмінити',
+	'userstatus-history' => 'Історія',
 );
+
