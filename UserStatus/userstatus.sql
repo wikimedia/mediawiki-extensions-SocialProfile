e10341991017f853ee4d4a378cddab1ea015e973
Antoine Musso <hashar@users.mediawiki.org>
2011-07-26 23:42:33 +0000

    put auto_increment at the end of declaration.
    stil have to figur eout the ON UPDATE CURRENT_TIMESTAMP error I am facing.

--- UserStatus/userstatus.sql
+++ UserStatus/userstatus.sql
@@ -1,6 +1,6 @@
 CREATE TABLE /*_*/user_status (
   -- Unique status ID number
-  `us_id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `us_id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   -- ID number of the user who wrote this status update
   `us_user_id` int(11) NOT NULL default '0',
   -- Timestamp of the status update
@@ -10,7 +10,7 @@ CREATE TABLE /*_*/user_status (
 )/*$wgDBTableOptions*/;
 
 CREATE TABLE /*_*/user_status_history (
-  `ush_id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `ush_id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `ush_user_id` int(11) NOT NULL default '0',
   `ush_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `ush_status` varchar(140) NOT NULL default ''
