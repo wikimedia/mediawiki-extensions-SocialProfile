6ddcd7ee651d92f2035841755f905903f2841459
Jack Phoenix <ashley@users.mediawiki.org>
2011-08-23 17:20:38 +0000

    SocialProfile: remove a superfluous comma in UserStatus' SQL file

--- UserStatus/userstatus.sql
+++ UserStatus/userstatus.sql
@@ -13,7 +13,7 @@ CREATE TABLE /*_*/user_status_history (
 	`ush_id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	`ush_user_id` int(11) NOT NULL default '0',
 	`ush_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-	`ush_status` varchar(140) NOT NULL default '',
+	`ush_status` varchar(140) NOT NULL default ''
 )/*$wgDBTableOptions*/;
 
 CREATE TABLE /*_*/user_status_likes (
