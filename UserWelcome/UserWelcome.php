ba7f09aaa5c01f6af862ac321cec891c70c2a930
Jack Phoenix <jack@countervandalism.net>
2013-12-16 17:48:06 +0200

    Fix HTML parsing in the 'mp-welcome-needed-points' i18n msg in UserWelcome.
    Change-Id: Ibb882519c17333ee3ac45aedbd28867ffd1f0fc3

--- UserWelcome/UserWelcome.php
+++ UserWelcome/UserWelcome.php
@@ -5,10 +5,10 @@
  *
  * @file
  * @ingroup Extensions
- * @version 1.4
+ * @version 1.4.1
  * @author David Pean <david.pean@gmail.com>
  * @author Jack Phoenix <jack@countervandalism.net>
- * @link http://www.mediawiki.org/wiki/Extension:UserWelcome Documentation
+ * @link https://www.mediawiki.org/wiki/Extension:UserWelcome Documentation
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
  */
 
@@ -20,7 +20,7 @@ if( !defined( 'MEDIAWIKI' ) ) {
 $wgExtensionCredits['parserhook'][] = array(
 	'path' => __FILE__,
 	'name' => 'UserWelcome',
-	'version' => '1.4',
+	'version' => '1.4.1',
 	'author' => array( 'David Pean', 'Jack Phoenix' ),
 	'descriptionmsg' => 'userwelcome-desc',
 	'url' => 'https://www.mediawiki.org/wiki/Extension:UserWelcome',
@@ -37,7 +37,8 @@ $wgResourceModules['ext.socialprofile.userwelcome.css'] = array(
 $wgHooks['ParserFirstCallInit'][] = 'wfWelcomeUser';
 /**
  * Register <welcomeUser /> tag with the parser
- * @param $parser Object: instance of Parser
+ *
+ * @param $parser Parser
  * @return Boolean: true
  */
 function wfWelcomeUser( &$parser ) {
@@ -109,7 +110,7 @@ function getWelcome() {
 					$level_link->escapeFullURL(),
 					$user_level->getNextLevelName(),
 					$user_level->getPointsNeededToAdvance()
-				)->parse() .
+				)->text() .
 			'</div>
 		</div>';
 	}
