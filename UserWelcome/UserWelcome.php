459ead8f6f3dc143c660cc531965e1b9452d752c
SamanthaNguyen <samanthanguyen1116@gmail.com>
2017-10-01 17:48:06 -0500

    Convert UserWelcome to use extension registration
    Bug: T152865
    Change-Id: Ia65ffc502ff03341df20f133bfecf44592a06f07

--- UserWelcome/UserWelcome.php
+++ UserWelcome/UserWelcome.php
@@ -1,41 +1,12 @@
 <?php
-/**
- * UserWelcome extension
- * Adds <welcomeUser/> tag to display user-specific social information
- *
- * @file
- * @ingroup Extensions
- * @version 1.5
- * @author David Pean <david.pean@gmail.com>
- * @author Jack Phoenix <jack@countervandalism.net>
- * @link https://www.mediawiki.org/wiki/Extension:UserWelcome Documentation
- * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
- */
-
-if( !defined( 'MEDIAWIKI' ) ) {
-	die( "This is not a valid entry point.\n" );
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'SocialProfile/UserWelcome' );
+	$wgMessagesDirs['UserWelcome'] = __DIR__ . '/i18n';
+	wfWarn(
+		'Deprecated PHP entry point used for UserWelcome. Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	);
+	return;
+} else {
+	die( 'This version of UserWelcome requires MediaWiki 1.29+.' );
 }
-
-// Extension credits that show up on Special:Version
-$wgExtensionCredits['parserhook'][] = array(
-	'path' => __FILE__,
-	'name' => 'UserWelcome',
-	'license-name' => 'GPL-2.0+',
-	'version' => '1.5',
-	'author' => array( 'David Pean', 'Jack Phoenix' ),
-	'descriptionmsg' => 'userwelcome-desc',
-	'url' => 'https://www.mediawiki.org/wiki/Extension:UserWelcome',
-);
-
-// Register the CSS with ResourceLoader
-$wgResourceModules['ext.socialprofile.userwelcome.css'] = array(
-	'styles' => 'UserWelcome.css',
-	'localBasePath' => __DIR__,
-	'remoteExtPath' => 'SocialProfile/UserWelcome',
-	'position' => 'top'
-);
-
-$wgAutoloadClasses['UserWelcome'] = __DIR__ . '/UserWelcomeClass.php';
-$wgMessagesDirs['UserWelcome'] = __DIR__ . '/i18n';
-
-$wgHooks['ParserFirstCallInit'][] = 'UserWelcome::onParserFirstCallInit';
