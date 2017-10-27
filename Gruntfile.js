b65e369f98fdbcf62d084d673987fdc21a321c0d
Umherirrender <umherirrender_de.wp@web.de>
2017-10-27 21:13:09 +0200

    build: Always exclude vendor and node_modules
    Prefer grunt config over ignore file
    
    Change-Id: I20d67ce0c6eb9695de7e10e404824555fa11cc1a

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,9 @@ module.exports = function ( grunt ) {
 			},
 			all: [
 				'**/*.js',
-				'!node_modules/**'
+				'!node_modules/**',
+				'!vendor/**',
+				'!shared/**'
 			]
 		},
 		banana: {
@@ -20,7 +22,8 @@ module.exports = function ( grunt ) {
 		jsonlint: {
 			all: [
 				'**/*.json',
-				'!node_modules/**'
+				'!node_modules/**',
+				'!vendor/**'
 			]
 		}
 	} );
