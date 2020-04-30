037590dd142bf3c068c81adf5742b3804f3a6b70
James D. Forrester <jforrester@wikimedia.org>
2020-04-30 12:30:40 -0700

    eslint: Remove general exemptions for no-undef and no-unused-vars and make pass
    Use the modern 'mw' and '$' references rather than the deprecated 'mediaWiki' and
    'jQuery' ones which RL might remove. Also, no need to wrap IIFEs with variables
    that RL gives us already.
    
    Change-Id: I9d6b36652db722b7532ffbf483e4f29b578cb058

--- UserBoard/resources/js/BoardBlast.js
+++ UserBoard/resources/js/BoardBlast.js
@@ -2,7 +2,7 @@ var BoardBlast = {
 	submitted: 0,
 
 	toggleUser: function ( user_id ) {
-		var elem = jQuery( '#user-' + user_id );
+		var elem = $( '#user-' + user_id );
 
 		if ( elem.hasClass( 'blast-friend-selected' ) ) {
 			elem.removeClass( 'blast-friend-selected' )
@@ -22,15 +22,15 @@ var BoardBlast = {
 	},
 
 	toggleType: function ( method, on, off ) {
-		var list = jQuery( '#blast-friends-list div.' + ( ( method === 1 ) ? off : on ) );
+		var list = $( '#blast-friends-list div.' + ( ( method === 1 ) ? off : on ) );
 
 		for ( var x = 0; x <= list.length - 1; x++ ) {
 			var el = list[ x ];
-			if ( jQuery( el ).hasClass( on ) || jQuery( el ).hasClass( off ) ) {
+			if ( $( el ).hasClass( on ) || $( el ).hasClass( off ) ) {
 				if ( method === 1 ) {
-					jQuery( el ).removeClass( off ).addClass( on );
+					$( el ).removeClass( off ).addClass( on );
 				} else {
-					jQuery( el ).removeClass( on ).addClass( off );
+					$( el ).removeClass( on ).addClass( off );
 				}
 			}
 		}
@@ -71,7 +71,7 @@ var BoardBlast = {
 		var selected = 0;
 		var user_ids_to = '';
 
-		var list = jQuery( '#blast-friends-list div.blast-friend-selected' );
+		var list = $( '#blast-friends-list div.blast-friend-selected' );
 		var el, user_id;
 		for ( var x = 0; x <= list.length - 1; x++ ) {
 			el = list[ x ];
@@ -80,7 +80,7 @@ var BoardBlast = {
 			user_ids_to += ( ( user_ids_to ) ? ',' : '' ) + user_id;
 		}
 
-		list = jQuery( '#blast-friends-list div.blast-foe-selected' );
+		list = $( '#blast-friends-list div.blast-foe-selected' );
 		for ( x = 0; x <= list.length - 1; x++ ) {
 			el = list[ x ];
 			selected++;
@@ -89,13 +89,13 @@ var BoardBlast = {
 		}
 
 		if ( selected === 0 ) {
-			window.alert( mediaWiki.msg( 'boardblast-js-error-missing-user' ) );
+			window.alert( mw.msg( 'boardblast-js-error-missing-user' ) );
 			BoardBlast.submitted = 0;
 			return 0;
 		}
 
 		if ( !document.getElementById( 'message' ).value ) {
-			window.alert( mediaWiki.msg( 'boardblast-js-error-missing-message' ) );
+			window.alert( mw.msg( 'boardblast-js-error-missing-message' ) );
 			BoardBlast.submitted = 0;
 			return 0;
 		}
@@ -104,46 +104,46 @@ var BoardBlast = {
 
 		document.blast.message.style.color = '#ccc';
 		document.blast.message.readOnly = true;
-		document.getElementById( 'blast-friends-list' ).innerHTML = mediaWiki.msg( 'boardblast-js-sending' );
+		document.getElementById( 'blast-friends-list' ).innerHTML = mw.msg( 'boardblast-js-sending' );
 		document.blast.submit();
 	}
 };
 
-jQuery( function () {
+$( function () {
 	// "Select/Unselect all" links
-	jQuery( 'div.blast-nav-links a.blast-select-all-link' ).on( 'click', function () {
+	$( 'div.blast-nav-links a.blast-select-all-link' ).on( 'click', function () {
 		BoardBlast.selectAll();
 	} );
 
-	jQuery( 'div.blast-nav-links a.blast-unselect-all-link' ).on( 'click', function () {
+	$( 'div.blast-nav-links a.blast-unselect-all-link' ).on( 'click', function () {
 		BoardBlast.unselectAll();
 	} );
 
 	// "Select/Unselect friends" links
-	jQuery( 'div.blast-nav-links a.blast-select-friends-link' ).on( 'click', function () {
+	$( 'div.blast-nav-links a.blast-select-friends-link' ).on( 'click', function () {
 		BoardBlast.toggleFriends( 1 );
 	} );
 
-	jQuery( 'div.blast-nav-links a.blast-unselect-friends-link' ).on( 'click', function () {
+	$( 'div.blast-nav-links a.blast-unselect-friends-link' ).on( 'click', function () {
 		BoardBlast.toggleFriends( 0 );
 	} );
 
 	// "Select/Unselect foes" links
-	jQuery( 'div.blast-nav-links a.blast-select-foes-link' ).on( 'click', function () {
+	$( 'div.blast-nav-links a.blast-select-foes-link' ).on( 'click', function () {
 		BoardBlast.toggleFoes( 1 );
 	} );
 
-	jQuery( 'div.blast-nav-links a.blast-unselect-foes-link' ).on( 'click', function () {
+	$( 'div.blast-nav-links a.blast-unselect-foes-link' ).on( 'click', function () {
 		BoardBlast.toggleFoes( 0 );
 	} );
 
 	// Toggling for an individual user
-	jQuery( 'div#blast-friends-list div[id^="user-"]' ).on( 'click', function () {
-		BoardBlast.toggleUser( jQuery( this ).attr( 'id' ).replace( /user-/, '' ) );
+	$( 'div#blast-friends-list div[id^="user-"]' ).on( 'click', function () {
+		BoardBlast.toggleUser( $( this ).attr( 'id' ).replace( /user-/, '' ) );
 	} );
 
 	// The submit button
-	jQuery( 'div.blast-message-box-button input[type="button"]' ).on( 'click', function () {
+	$( 'div.blast-message-box-button input[type="button"]' ).on( 'click', function () {
 		BoardBlast.sendMessages();
 	} );
 } );
--- UserGifts/resources/js/UserGifts.js
+++ UserGifts/resources/js/UserGifts.js
@@ -4,26 +4,26 @@ var UserGifts = {
 	selectGift: function ( id ) {
 		// Un-select previously selected gift
 		if ( UserGifts.selected_gift ) {
-			jQuery( '#give_gift_' + UserGifts.selected_gift ).removeClass( 'g-give-all-selected' );
+			$( '#give_gift_' + UserGifts.selected_gift ).removeClass( 'g-give-all-selected' );
 		}
 
 		// Select new gift
-		jQuery( '#give_gift_' + id ).addClass( 'g-give-all-selected' );
+		$( '#give_gift_' + id ).addClass( 'g-give-all-selected' );
 
 		UserGifts.selected_gift = id;
 	},
 
 	highlightGift: function ( id ) {
-		jQuery( '#give_gift_' + id ).addClass( 'g-give-all-highlight' );
+		$( '#give_gift_' + id ).addClass( 'g-give-all-highlight' );
 	},
 
 	unHighlightGift: function ( id ) {
-		jQuery( '#give_gift_' + id ).removeClass( 'g-give-all-highlight' );
+		$( '#give_gift_' + id ).removeClass( 'g-give-all-highlight' );
 	},
 
 	sendGift: function () {
 		if ( !UserGifts.selected_gift ) {
-			window.alert( mediaWiki.msg( 'g-select-gift' ) );
+			window.alert( mw.msg( 'g-select-gift' ) );
 			return false;
 		}
 		document.gift.gift_id.value = UserGifts.selected_gift;
@@ -33,38 +33,38 @@ var UserGifts = {
 	chooseFriend: function ( friend ) {
 		// Now, this is a rather nasty hack since the original (commented out below) wouldn't work when $wgScriptPath was set
 		//window.location = window.location + "&user=" + friend;
-		window.location = mediaWiki.config.get( 'wgServer' ) + mediaWiki.config.get( 'wgScript' ) +
+		window.location = mw.config.get( 'wgServer' ) + mw.config.get( 'wgScript' ) +
 			'?title=Special:GiveGift' + '&user=' + friend;
 	}
 };
 
-jQuery( function () {
+$( function () {
 	// "Select a friend" dropdown menu
-	jQuery( 'div.g-gift-select select' ).on( 'change', function () {
-		UserGifts.chooseFriend( jQuery( this ).val() );
+	$( 'div.g-gift-select select' ).on( 'change', function () {
+		UserGifts.chooseFriend( $( this ).val() );
 	} );
 
 	// Handlers for individual gift images
-	jQuery( 'div[id^=give_gift_]' ).on( {
+	$( 'div[id^=give_gift_]' ).on( {
 		'click': function () {
 			UserGifts.selectGift(
-				jQuery( this ).attr( 'id' ).replace( 'give_gift_', '' )
+				$( this ).attr( 'id' ).replace( 'give_gift_', '' )
 			);
 		},
 		'mouseout': function () {
 			UserGifts.unHighlightGift(
-				jQuery( this ).attr( 'id' ).replace( 'give_gift_', '' )
+				$( this ).attr( 'id' ).replace( 'give_gift_', '' )
 			);
 		},
 		'mouseover': function () {
 			UserGifts.highlightGift(
-				jQuery( this ).attr( 'id' ).replace( 'give_gift_', '' )
+				$( this ).attr( 'id' ).replace( 'give_gift_', '' )
 			);
 		},
 	} );
 
 	// "Send gift" button
-	jQuery( 'input#send-gift-button' ).on( 'click', function () {
+	$( 'input#send-gift-button' ).on( 'click', function () {
 		UserGifts.sendGift();
 	} );
 } );
